41afa0e39597409d68c4a8c606a42674dd4e2058
toskaMaliske <55053100+toskaMaliske@users.noreply.github.com>
2019-10-31 12:33:05 +0100

    updated trigger

--- sources/game.js
+++ sources/game.js
@@ -12,60 +12,62 @@ class Game {
 
     // SETUP PLAYER (+ MODES?)
     setup() {
-        // mode = 0;
         this.player.setup();
     }
 
 
     //DRAW BACKGROUND, PLAYER & OBSTACLE 
     draw() {
-        // clear();
+        clear();
         if (mode == 0) {
-            textSize(21);
+            background('#222222');
+            textAlign(CENTER);
+            textSize(25);
             let welcomeText = "Welcome to ";
-            let startText = "Press space to start the game!";
-            textSize(30);
-            let startTitle = "Surviving Web Dev Bootcamp. ";
+            text(welcomeText, width / 3, height / 3, width / 3, height / 3);
 
-            text(welcomeText + startTitle + startText, width / 3, height / 3, width / 3, height / 3);
+            textSize(30);
+            let startTitle = "Web Dev Bootcamp 101 ";
+            text(startTitle, width / 3, height / 2.5, width / 3, height / 2.5);
+
+            let startText = "Press SPACE to start the game!";
+            textSize(25);
+            text(startText, width / 3, height / 2, width / 3, height / 2);
+            fill("white");
+
+
+
         }
         if (mode == 1) {
 
             this.background.draw();
             this.player.draw();
 
-
-            // if (mode == 2) {
-            //     //triggered by quests
-            //     //this.background.noLoop();
-            //     //use input + button p5 (do checking of questions with multiple choice - check value)
-            // }
-            // if (mode == 3) {
-            //     //game over + score screen 
-            // }
-            //==========================================================
-
-
             text("Score: " + this.player.score, width - 200, height - 800); //y-position not working?
             text("Lives: " + this.player.lives, width - 200, height - 750);
             fill(255, 255, 255);
+
+
 
             if (frameCount > 480 && frameCount % 240 === 0) {
                 this.quests.push(new Quests());
                 //console.log("create new quests");
             }
 
+
             this.quests.forEach((quest, index) => {
                 // console.log("----", quest)
                 quest.draw();
+
                 if (quest.x + quest.width <= 1 && quest.y + quest.height >= 1) {
-                    // if (quest.x - width <= 0 || quest.x + width >= width || quest.y - height <= 0 || quest.y + height >= height) {
                     this.quests.splice(index, 1);
                 }
+
                 if (this.playerCollision(quest, this.player)) {
                     console.log("Quest Collision");
-                    // this.player.score++;
-                    noLoop();
+                    this.quests.splice(index, 1);
+                    this.player.score += 1;
+                    this.triggerQuiz();
                 }
 
             });
@@ -84,35 +86,110 @@ class Game {
                 }
                 if (this.playerCollision(obstacle, this.player)) {
                     console.log("Obstacle collision");
-                    // this.player.lives -= 1;
-                    noLoop();
-                    // if (this.player.lives == 0) {
-                    //     mode = 3;
-                    //}
+                    console.log(this.player.x, this.player.y);
+                    console.log(obstacle.x, obstacle.y);
+                    this.obstacles.splice(index, 1);
+                    this.player.lives -= 1;
                 }
 
+                if (this.player.lives === 0) {
+                    mode = 3;
+                }
+                // }
+
             });
+
+
+            //end of mode 1 ===========================================================            
         }
+
+        if (mode == 3) {
+
+            background('#222222');
+            textAlign(CENTER);
+            textSize(30);
+            let gameOverTitle = "Game Over";
+            text(gameOverTitle, width / 3, height / 3, width / 3, height / 3);
+
+            let restart = "You're final score: ";
+            text(restart + this.player.score, width / 3, height / 2, width / 3, height / 2);
+            fill("white");
+
+            // let restart = "Press SPACE to restart the game";
+            // text(restart, width / 3, height / 2, width / 3, height / 2);
+            // fill("white");
+
+            keyPressed(); //restart the game
+
+
+        }
+
+
+        //End of draw()=============================================================
     }
 
 
+
     playerCollision(item, player) {
-        if (player.x + player.width <= item.x || item.x + player.width <= player.x) { //try to set half of the width so that collision is not occuring to early  
+        if (player.x + player.width < item.x || item.x + item.width < player.x) { //try to set half of the width so that collision is not occuring to early  
             return false;
         }
         if (player.y > item.y + item.height || item.y > player.y + player.height) {
             return false;
         }
         return true;
+    }
 
+    triggerQuiz() {
+
+
+        // input = createSelect();
+        input = createInput();
+        input.position(20, 65);
+
+        button = createButton('submit');
+        button.position(input.x + input.width, 65);
+        button.mousePressed(this.triggerQuiz);
+
+        greeting = createElement('h2', 'Solve a Javascript question to earn 2 score points');
+        gretting2 = createElement("h3", random(quiz.prompt));
+        greeting.position(20, 5);
+        fill(255, 255, 255);
+        textAlign(CENTER);
+        textSize(50);
+
+
+        //     for (let i = 0; i < quiz.length; i++) {
+        //         let response = input.value(quiz[i].prompt);
+        //         if (response == quiz[i].answer) {
+        //             this.player.score += 2;
+        //         }
+        // }
+        button.mousePressed(clear(this.triggerQuiz));
     }
 
 
-    //=============================================================
+
+    //end of game()=============================================================    
 }
 
 
 
-//TRIGGER QUESTS
+
+
+
+
+
+//==========================================================
+
 
 //CHECK IF QUESTS WERE ANSWERED CORRECTLY, ADD POINTS TO SCORE / SUBTRACT LIVES
+
+
+// for (let i = 0; i < quiz.length; i++) {
+//     let response = window.prompt(quiz[i].prompt);
+//     if (response == quiz[i].answer) {
+//         console.log("right!");
+//         // this.player.score++;
+//     }
+// }
